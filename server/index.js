5d499320be2d4dfba82b56724d5f5d32b01dbcb0
GREEDnim <sivadhas007@gmail.com>
2023-07-13 06:07:30 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const cors=require('cors')
+require('dotenv').config()
+const mongoose=require('mongoose');
+const app=express();
+
+app.use(cors());
+
+
+const PORT=process.env.PORT || 5000;
+const MONGO_URL=process.env.MONGO_URL ;
+mongoose.connect(MONGO_URL)
+    .then(()=>{
+        console.log("CONNECTED TO DB");
+        app.listen(PORT,()=>{
+            console.log("BACKEND RUNNING AT PORT : "+PORT);
+        }) 
+    })
+    .catch((e=>{
+        console.log(e.message);
+    }))
+        
+    

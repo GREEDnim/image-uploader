b8e018882ee35fa1b7517916a2f4c790639f702a
GREEDnim <sivadhas007@gmail.com>
2023-07-17 01:23:01 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,28 +2,32 @@ import { useState,useRef } from "react"
 import InputCard from "./Components/InputCard/InputCard"
 import Loading from "./Components/Loader/Loading"
 import OutputCard from "./Components/OutputCard/OutputCard"
+import Footer from "./Components/Footer/Footer"
 import './App.css'
 import {DisplayContext} from "./Contexts/DisplayContext"
 
 function App() {
+
   const[whatToDisplay,changeWhatToDisplay]=useState(0);
+
   const [url,setUrl]=useState('');
   let localUrl=useRef({});
 
   function getComponentToLoad(){
-      if(whatToDisplay==1) return <Loading ></Loading>;
+      if(whatToDisplay==1) return <Loading></Loading>;
       if(whatToDisplay==2) return <OutputCard></OutputCard>;
       else return <InputCard  ></InputCard>
   }
 
   return (
 
-    
     <div className="App">
-      <DisplayContext.Provider value={{changeWhatToDisplay,url,setUrl,localUrl}}>
-        {getComponentToLoad()}  
-      </DisplayContext.Provider>
-       
+      <div className="cards">
+          <DisplayContext.Provider value={{changeWhatToDisplay,url,setUrl,localUrl}}>
+            {getComponentToLoad()}  
+          </DisplayContext.Provider>
+      </div>
+      <Footer></Footer>   
     </div>
   )
 }
